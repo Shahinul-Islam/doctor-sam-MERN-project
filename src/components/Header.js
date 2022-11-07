9ea858fa6fc98cf1ffb0e46082bdbd8401d7c2df
shahinul islam <shahinul37@outlook.com>
2022-11-08 04:06:07 +0600

    initaial setup done

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Header = () => {
+  return (
+    <div>
+      <h2>thsi is header</h2>
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default Header;
