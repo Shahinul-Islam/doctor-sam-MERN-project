729a7be708ab2ecb6206e2b10e457c1631e6d197
shahinul islam <shahinul37@outlook.com>
2022-11-08 08:52:43 +0600

    services fetched from backen

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // import Navbar from "./Navbar";
 
 const Header = () => {
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/services/")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
   return (
-    <div
-      className="hero"
-      style={{
-        backgroundImage: `url("https://images.pexels.com/photos/1250655/pexels-photo-1250655.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")`,
-      }}
-    >
-      <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Welcome to Doctor Sam</h1>
-          <p className="mb-5">Eye Specialist & Phaco Surgeon</p>
-          <button className="btn btn-secondary">Get an Appointment</button>
+    <div>
+      <div
+        className="hero h-96"
+        style={{
+          backgroundImage: `url("https://images.pexels.com/photos/1250655/pexels-photo-1250655.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Welcome to Doctor Sam</h1>
+            <p className="mb-5">Eye Specialist & Phaco Surgeon</p>
+            <button className="btn btn-secondary">Get an Appointment</button>
+          </div>
         </div>
+      </div>
+      <div>
+        {services.map((service) => (
+          <p key={service._id}>{service.title}</p>
+        ))}
       </div>
     </div>
   );
---
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthProvider = () => {
+  return (
+    <div>
+      <h2>context to share authentication</h2>
+    </div>
+  );
+};
+
+export default AuthProvider;
